64ded0ec724a65e4587d92463d1714d61af54992
akuusong-cell <akuu.song@gmail.com>
2026-03-09 15:30:07 +0900

    후후
    이잉

---
+++ b1.js
@@ -0,0 +1,14 @@
+const a = 10;
+console.log(a);
+
+// javascript 
+//브라우저, 터미널에서 돌릴 수 있음
+//터미널 - 백엔드 브라우저 - 프론트엔드
+//브라우저에서 실행하려면 스크립트 태그? 로 감싸야함
+//자동정렬 = option + shift + f
+//<script src="b1.js"></script> html에서 이 파일을 불러옴
+
+//<script>
+//      const b =20;
+//      console.log(b);
+//</script>
